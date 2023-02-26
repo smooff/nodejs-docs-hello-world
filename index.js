4f29be4c17ec95293c0ed3da173aa9cb29b802ec
smooff <57942620+smooff@users.noreply.github.com>
2023-02-27 00:51:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // ***************************************************************************
 
 response.end("Hello World!");
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
